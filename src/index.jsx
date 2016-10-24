c2d372dd221fd18a84502b489cf856c715a9a85e
Cheng Ly <chengsieuly@gmail.com>
2016-10-23 21:46:26 -0700

    feat: Initial React setup

---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import App from './App';
+
+// Need to hook our app onto something
+render(<App />, document.getElementById('root'));
