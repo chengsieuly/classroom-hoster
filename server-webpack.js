b41b1f72b5003e13422a919f863d342856a972ad
Cheng Ly <chengsieuly@gmail.com>
2016-10-24 18:54:23 -0700

    build: Update webpack to handle proxys

--- server-webpack.js
+++ server-webpack.js
@@ -3,6 +3,9 @@ const WebpackDevServer = require('webpack-dev-server');
 const config = require('./webpack.config');
 
 new WebpackDevServer(webpack(config), {
+  proxy: {
+    '**': { target: 'http://localhost:3000', secure: false },
+  },
   contentBase: config.output.path,
   colors: true,
   historyApiFallback: true,
@@ -12,8 +15,8 @@ new WebpackDevServer(webpack(config), {
   progress: true,
   quiet: false,
   stats: 'minimal',
-}).listen(3000, 'localhost', (err) => {
+}).listen(8080, 'localhost', (err) => {
   if (err) return console.log(err);
 
-  console.log('Listening at http://localhost:3000/');
+  console.log('Listening at http://localhost:8080/');
 });
