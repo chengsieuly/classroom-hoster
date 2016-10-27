392d842048782ef3badce98ed13936339120263a
Cheng Ly <chengsieuly@gmail.com>
2016-10-26 23:54:30 -0700

    feat: Reroute after quiz creation

--- src/modules/App/App.jsx
+++ src/modules/App/App.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
 import Quiz from 'components/Quiz/Quiz';
 
 export default class App extends Component {
@@ -18,11 +19,24 @@ export default class App extends Component {
       });
   }
 
+  host(id) {
+    alert(`Student can now join ${window.location.host}/quiz/${id}`);
+    window.location = `/host/${id}`;
+  }
+
   render() {
     return (
-      <div>
-        {this.state.data.map(({ title, description, questions }, i) => (
-          <Quiz key={i} title={title} description={description} questions={questions} />
+      <div className="container">
+        <Link to="/create" className="btn btn-primary">Create Quiz</Link>
+        {this.state.data.map(({ id, title, description, questions }, i) => (
+          <Quiz
+            key={i}
+            quizId={`${id}`}
+            title={title}
+            description={description}
+            questions={questions}
+            host={this.host}
+            />
         ))}
       </div>
     );
